744292b1b71f25ebb2496ae3acb73c1befb6a038
YOOYONGHYUN <yyh901@naver.com>
2022-02-17 17:36:07 +0900

    git

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
-
+const PORT = 4000;
 const mongoose = require("mongoose");
+const { User } = require("./models/user");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 mongoose
   .connect(
@@ -11,5 +15,13 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello"));
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
+
 app.listen(PORT, () => console.log(`You are listening to ${PORT}`));
