532ab78c693ece5f93c85bc9a7ab0031d60bf8f9
YOOYONGHYUN <yyh901@naver.com>
2022-02-17 10:57:29 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://YOOYONGHYUN:Dufgksk4109!@cluster0.fvufd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(PORT, () => console.log(`You are listening to ${PORT}`));
